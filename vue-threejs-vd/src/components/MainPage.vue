755c1e2c1cff1d9b32312069933fd7fd5df99c0e
Ash Zhang <zblash@outlook.com>
2021-02-19 01:38:05 +1100

    init the proejct and build the very basic Three js project

---
+++ vue-threejs-vd/src/components/MainPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- <h1>Hello, world!</h1> -->
+  <body>
+    <div class="container">
+      <nav>
+          hi
+      </nav>
+      <three-interface>
+      </three-interface>
+    </div>
+  </body>
+</template>
+
+<script>
+import ThreeInterface from './ThreeInterface.vue';
+export default {
+  name: 'MainPage',
+  components: {
+      ThreeInterface
+  },
+};
+</script>
+
+<style>
+
+.navbar {
+background: #ff7b30;
+margin-bottom: -21px !important;
+height: 80px;
+line-height: 80px;
+font-size: 24px;
+color: white;
+padding-left: 38px;
+}
+
+</style>
